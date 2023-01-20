a158b7c6e0a9e0523e66dc898d478b471a40247d
minglu-xie <mingluxie@gmail.com>
2023-01-20 11:33:08 +0100

    Record of whole exome-sequencing analysis

---
+++ record.sh
@@ -0,0 +1,6 @@
+#################################################
+#  File Name:record.sh
+#  Author: Minglu.Xie
+#  Mail: minglu.xie.1023@student.uu.se,mingluxie@gmail.com
+#  Created Time: Fri 06 Jan 2023 01:07:36 PM CET
+#################################################
