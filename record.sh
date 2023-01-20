727db3a8fc8f9bd1b155660108ac05ee96c4e130
minglu-xie <mingluxie@gmail.com>
2023-01-20 11:50:44 +0100

    test

--- record.sh
+++ record.sh
@@ -4,3 +4,8 @@
 #  Mail: minglu.xie.1023@student.uu.se,mingluxie@gmail.com
 #  Created Time: Fri 06 Jan 2023 01:07:36 PM CET
 #################################################
+
+# create the environment by conda/mamba.
+mamba create -n wes
+mamba activate wes
+mamba install bcftools snpEFF multiqc qualimap
